1f67c2d05d004fedfc44ac00408ff4ce5b17c0a0
richardcreatech <richardcreatech@gmail.com>
2025-01-15 09:11:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+alert(`This site typically utilizes Objects and built in functions such as Alert. This was done because I've been trying to learn object oriented programming and this acts by storing the info typed in the input in an object - {}`)
 
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
